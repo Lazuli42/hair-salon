9dd09576c4c1e8d1e327cb5f113bc1e7930fc739
Zachary Baum <zacharybaum42@gmail.com>
2016-09-23 10:06:12 -0400

    create Client class and test. add save, getAll, and deleteAll methods to Client

---
+++ src/Client.php
@@ -0,0 +1,61 @@
+<?php
+    class Client
+    {
+        private $id;
+        private $name;
+        private $stylist_id;
+
+        function __construct($id=null, $name, $stylist_id)
+        {
+            $this->id = $id;
+            $this->name = $name;
+            $this->stylist_id = $stylist_id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getStylistId()
+        {
+            return $this->stylist_id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO clients (name, stylist_id) VALUES ('{$this->getName()}', {$this->getStylistId()});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients;");
+            $clients = array();
+            foreach($returned_clients as $client) {
+                $id = $client['id'];
+                $name = $client['name'];
+                $stylist_id = $client['stylist_id'];
+                $new_client = new Client($id, $name, $stylist_id);
+                array_push($clients, $new_client);
+            }
+            return $clients;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM clients;");
+        }
+    }
+
+ ?>
